5ee20bafa9ec5d1f0837f9c277a3123080d0eb06
JaredGlaub <jglaub@wisc.edu>
2020-03-01 17:31:56 -0600

    Milestone 2 completed

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,13 +1,48 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.Context;
+//import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        EditText username = (EditText) findViewById(R.id.editText);
+        EditText password = (EditText) findViewById(R.id.editText2);
+        String str = username.getText().toString();
+        String str2 = password.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username",str).apply();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("username", s);
+
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String key = "username";
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+
+        if (!sharedPreferences.getString(key, "").equals("")) {
+            String user = sharedPreferences.getString(key, "");
+            goToActivity2(user);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 }
